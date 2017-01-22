23bdcb23e5ce4eabb252ca021bc8ceb89714231e
Sondre <sondre.fjeldheim@svarverud.com>
2017-01-22 02:36:21 +0100

    healthbar

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -8,13 +8,17 @@ public class PlayerHealth : MonoBehaviour, ITakeDamage {
     private float curHealth;
     private bool dead = false;
     private Text textReference;
-    
-	// Use this for initialization
-	void Start () {
+    //List<Sprite> HealthSprites = new List<Sprite>();
+
+    [SerializeField]
+    public Sprite[] healthSprites;
+
+    // Use this for initialization
+    void Start () {
         curHealth = maxHealth;
-        textReference = GameObject.Find("HealthText").GetComponent<Text>();
-	}
-	
+        textReference = GameObject.Find("Text").GetComponent<Text>();
+    }
+
     public void TakeDamage(float damage) {
         if(dead) {
             return;
@@ -30,7 +34,9 @@ public class PlayerHealth : MonoBehaviour, ITakeDamage {
 	// Update is called once per frame
 	void Update () {
         if(dead) {
-            Destroy(gameObject);
+            GameObject.Find("Healthbar").GetComponent<Image>().sprite = healthSprites[0];
+            Destroy(gameObject, 5);
+            Destroy(GameObject.Find("Healthbar"), 5);
             GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled = true;
 
             //Set text and alignment canvas, but idk why it doesn't work properly with position
@@ -42,12 +48,8 @@ public class PlayerHealth : MonoBehaviour, ITakeDamage {
         }
 
         if(!dead) {
-            textReference.alignment = TextAnchor.LowerCenter;
-            textReference.rectTransform.anchorMin = new Vector2(0.5f, 0.0f);
-            textReference.rectTransform.anchorMax = new Vector2(0.5f, 0.0f);
-            textReference.rectTransform.position.Set(0.0f, 40.0f, 0.0f);
-            textReference.text = "HEALTH: " + curHealth;
+            GameObject.Find("Healthbar").GetComponent<Image>().sprite = healthSprites[(int)(curHealth / 10)];
         }
-        
+
     }
 }
